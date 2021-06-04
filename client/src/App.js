8c3ef4176b6dad213890bc086172db255626c241
NataliaGatarek <nova.nathalia@gmail.com>
2021-06-04 12:17:34 +0200

    commit

---
+++ client/src/App.js
@@ -0,0 +1,14 @@
+import SignIn from "./Views/SignIn.js";
+import SignUp from "./Views/SignUp.js";
+import UpIn from "./Views/SignUp.js";
+
+function App() {
+  return (
+    <div>
+      <SignIn />
+      <SignUp/>
+    </div>
+  );
+}
+
+export default App;
