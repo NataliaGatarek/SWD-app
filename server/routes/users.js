2047df217c4bd009743bfeb4500a4cc3488503b6
NataliaGatarek <nova.nathalia@gmail.com>
2021-06-10 16:57:17 +0200

    user registration, save and encrypt

--- server/routes/users.js
+++ server/routes/users.js
@@ -1,7 +1,56 @@
 const express = require("express");
 const router = express.Router();
-const usersModel = require("../models/usersModel");
-router.get("/test", (req, res) => {
+const User = require("../models/usersModel");
+const { body, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
+
+router.post(
+  "/",
+  body("firstName", "Name is required").notEmpty(),
+  body("lastName", "Last name is required").notEmpty(),
+  body("email", "Please include a valid email").isEmail(),
+  body(
+    "password",
+    "Please enter a password with 6 or more characters"
+  ).isLength({ min: 6 }),
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+    const { firstName, lastName, email, password } = req.body;
+    try {
+      //see if user exists
+      let user = await User.findOne({ email });
+      //search by something, here search by email because its unique
+      if (user) {
+        res.user
+          .status(400)
+          .json({ errors: [{ message: "User already exists" }] });
+        //this set up of the error makes sure that errors are same on client and server side
+      }
+      //enctpt the password
+      //1.creating salt to hash the password
+      const salt = await bcrypt.genSalt(10);
+      const hashedPw = await bcrypt.hash(req.body.password, salt);
+
+      //new user
+      const newUser = new User({
+        firstName: req.body.firstName,
+        lastName: req.body.lastName,
+        email: req.body.email,
+        password: hashedPw,
+      });
+      const usero = await newUser.save();
+      //return json token
+      res.send("User registered");
+    } catch (error) {}
+    console.log(error.message);
+    res.status(500).send("Server error");
+  }
+);
+
+/* router.get("/test", (req, res) => {
   usersModel.find({}, function (err, users) {
     if (err) {
       res.send(err);
@@ -9,9 +58,9 @@ router.get("/test", (req, res) => {
       res.send(users);
     }
   });
-});
+}); */
 
-router.get("/:id", (req, res) => {
+/* router.get("/:id", (req, res) => {
   let usersId = req.params.id;
   usersModel
     .findById(usersId)
@@ -25,6 +74,10 @@ router.get("/:id", (req, res) => {
         res.send(users);
       }
     });
-});
+});  */
+
+// @route    POST api/users
+// @desc     Register user
+// @access   Public
 
 module.exports = router;
