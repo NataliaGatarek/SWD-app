60cf80c56c4ba0aa441ae5938cb076f077ef8564
NataliaGatarek <nova.nathalia@gmail.com>
2021-06-19 12:21:27 +0200

    design, profile

--- client/src/Views/Profile.js
+++ client/src/Views/Profile.js
@@ -1,11 +1,8 @@
 import React from "react";
 import { makeStyles } from "@material-ui/core/styles";
-import Accordion from "@material-ui/core/Accordion";
-import AccordionDetails from "@material-ui/core/AccordionDetails";
-import AccordionSummary from "@material-ui/core/AccordionSummary";
 import Typography from "@material-ui/core/Typography";
-import ExpandMoreIcon from "@material-ui/icons/ExpandMore";
 import PetsIcon from "@material-ui/icons/Pets";
+import Paper from "@material-ui/core/Paper";
 import {
   BrowserRouter as Router,
   Switch,
@@ -15,25 +12,19 @@ import {
 } from "react-router-dom";
 const useStyles = makeStyles((theme) => ({
   root: {
-    width: "100%",
-  },
-  heading: {
-    fontSize: theme.typography.pxToRem(15),
-    flexBasis: "33.33%",
-    flexShrink: 0,
-  },
-  secondaryHeading: {
-    fontSize: theme.typography.pxToRem(15),
-    color: theme.palette.text.secondary,
+    display: "flex",
+    flexDirection: "column",
+    flexWrap: "wrap",
+    "& > *": {
+      margin: theme.spacing(1),
+      width: theme.spacing(45),
+      height: theme.spacing(32),
+    },
   },
 }));
 function Profile() {
   const classes = useStyles();
-  const [expanded, setExpanded] = React.useState(false);
 
-  const handleChange = (panel) => (event, isExpanded) => {
-    setExpanded(isExpanded ? panel : false);
-  };
   return (
     <div>
       <h1 className="header-style">Welcome to your profile</h1>
@@ -44,63 +35,37 @@ function Profile() {
           <PetsIcon />{" "}
         </Link>
       </h2>
-      <div className={classes.root} style={{ textAlign: "center" }}>
-        <Accordion
-          expanded={expanded === "panel1"}
-          onChange={handleChange("panel1")}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel1bh-content"
-            id="panel1bh-header"
+      <div className={classes.root}>
+        <Paper elevation={3}>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
           >
-            <Typography className={classes.heading}>
-              Your information
-            </Typography>
-            <Typography className={classes.secondaryHeading}>
-              Welcome NAME
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>Name, Last name, email</Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion
-          expanded={expanded === "panel2"}
-          onChange={handleChange("panel2")}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel2bh-content"
-            id="panel2bh-header"
+            Your information
+          </Typography>
+        </Paper>
+        <Paper elevation={3}>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
           >
-            <Typography className={classes.heading}>Your favorites</Typography>
-            <Typography className={classes.secondaryHeading}>
-              View list favorite dogs
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>Links to profiles</Typography>
-          </AccordionDetails>
-        </Accordion>
-        <Accordion
-          expanded={expanded === "panel3"}
-          onChange={handleChange("panel3")}
-        >
-          <AccordionSummary
-            expandIcon={<ExpandMoreIcon />}
-            aria-controls="panel3bh-content"
-            id="panel3bh-header"
+            Your favorites
+          </Typography>
+        </Paper>
+        <Paper elevation={3}>
+          <Typography
+            variant="h5"
+            align="center"
+            color="textSecondary"
+            paragraph
           >
-            <Typography className={classes.heading}>My own dogs</Typography>
-            <Typography className={classes.secondaryHeading}>
-              See the list of your own dogs
-            </Typography>
-          </AccordionSummary>
-          <AccordionDetails>
-            <Typography>List of your dogs</Typography>
-          </AccordionDetails>
-        </Accordion>
+            Your dogs
+          </Typography>
+        </Paper>
       </div>
     </div>
   );
