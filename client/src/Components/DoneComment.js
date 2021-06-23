3f97e180a11382804d9351d384bcdc7d62b153d5
NataliaGatarek <nova.nathalia@gmail.com>
2021-06-23 17:25:05 +0200

    Deleting comment front end

--- client/src/Components/DoneComment.js
+++ client/src/Components/DoneComment.js
@@ -5,7 +5,6 @@ import "../Views/views.css";
 import { Typography } from "@material-ui/core";
 
 
-function Comment(props) {
     return (
         <div>
             <Paper elevation={3} style={{"height": "100px", "width":"300px"}}>
