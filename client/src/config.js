290322739753952c21241eda28cc58c6a8320425
NataliaGatarek <nova.nathalia@gmail.com>
2021-07-09 12:55:59 +0200

    update

--- client/src/config.js
+++ client/src/config.js
@@ -1,4 +1,7 @@
-serverURL:
+ 
+    module.exports = {
+  serverURL:
     process.env.NODE_ENV === "development"
       ? "http://localhost:5000"
-      : "https://<https://swd-app.herokuapp.com/>"
+    : "https://<https://swd-app.herokuapp.com/>"
+};
